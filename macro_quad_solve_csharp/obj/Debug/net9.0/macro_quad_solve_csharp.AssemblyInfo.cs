76882e64a6e58e3b21b7ff31eb1b1262748ee312
serenebliss0 <semajayi1234@gmail.com>
2026-01-10 14:39:20 +0100

    Just some random code. I don't really have much time these days...

--- macro_quad_solve_csharp/obj/Debug/net9.0/macro_quad_solve_csharp.AssemblyInfo.cs
+++ macro_quad_solve_csharp/obj/Debug/net9.0/macro_quad_solve_csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("macro_quad_solve_csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e965554987df69b8ea37d8210dc68366bf0f8aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6fd36c9556e08169b56faf5f02ac51ae0e0c063")]
 [assembly: System.Reflection.AssemblyProductAttribute("macro_quad_solve_csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("macro_quad_solve_csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
